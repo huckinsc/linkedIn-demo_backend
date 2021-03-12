5421f72b3d3c48e0576cc52afd7f3f98403003ac
Chris Huckins <huckinsc@amazon.com>
2021-03-12 18:14:15 -0500

    Init commit

---
+++ db/index.js
@@ -0,0 +1,40 @@
+const students = [
+  {
+    id: "45",
+    name: "David Kwon",
+    role: "Software Development Engineer",
+    company: "Amazon",
+    linkedIn: "https://www.linkedin.com/in/david-kwon-4470a91aa/",
+    picture:
+      "https://media-exp1.licdn.com/dms/image/C5603AQHsVJ444BVWAA/profile-displayphoto-shrink_400_400/0/1589415989539?e=1620864000&v=beta&t=mAXxdFMoZxJDIzzxGMQiqy6t_HfK8fbsIgrchDajJak",
+  },
+  {
+    id: "464",
+    name: "Kathy Le",
+    role: "Software Development Engineer",
+    company: "Audible",
+    linkedIn: "https://www.linkedin.com/in/kathyle-dev/",
+    picture:
+      "https://media-exp1.licdn.com/dms/image/C4E03AQH7WQjeT0eHSw/profile-displayphoto-shrink_800_800/0/1587658737875?e=1620864000&v=beta&t=EEY4U-cK5BLD2Zg7irwqVHNjmec23gtjZH_niudtyhE",
+  },
+  {
+    id: "147",
+    name: "Jasmine Meade",
+    role: "Software Developer Engineer Apprentice",
+    company: "Amazon",
+    linkedIn: "https://www.linkedin.com/in/jasmine-meade/",
+    picture:
+      "https://media-exp1.licdn.com/dms/image/C4D03AQEmGnD6oDh-CQ/profile-displayphoto-shrink_800_800/0/1597330418085?e=1620864000&v=beta&t=9YQDVlbEA3fnjcwFQV_zY6H1mpiO1xejx8IVVetK-X4",
+  },
+  {
+    id: "357",
+    name: "Chris Huckins",
+    role: "SDE Apprentice",
+    company: "Amazon",
+    linkedIn: "https://www.linkedin.com/in/chris-huckins-93217488/",
+    picture:
+      "https://media-exp1.licdn.com/dms/image/C5603AQGeaJAWh1tnuw/profile-displayphoto-shrink_200_200/0/1614726883443?e=1620864000&v=beta&t=7QqNEVFGhSml2SM3_qVZVDlQwK31kBgQEsaEEv5_r88",
+  },
+];
+
+module.exports = students;
---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const server = express();
+
+const cors = require("cors");
+
+server.use(express.json());
+const PORT = 8080;
+
+server.use(cors());
+
+const students = require("./db");
+
+server.get("/students", (req, res) => {
+  res.send(students);
+});
+
+server.post("/students", (req, res) => {
+  //console.log(req.body);
+  students.push(req.body);
+  res.sendStatus(200);
+});
+
+server.delete("/students", (req, res) => {
+  //console.log(req.body);
+  //console.log(req.body.position);
+  students.splice(req.body.position, 1);
+  res.sendStatus(200);
+});
+
+server.put("/students", (req, res) => {
+  //console.log(req.body);
+  let found = false;
+  for (let i = 0; i < students.length; i++) {
+    if (students[i].id === req.body.id) {
+      students[i] = req.body;
+    }
+  }
+  res.send(students);
+});
+
+server.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
